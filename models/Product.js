87e873fbfcc53d50121943a24ebf05a9595260dd
St.Onge <stonge.ms@gmail.com>
2021-07-06 20:02:57 -0700

    added notes

--- models/Product.js
+++ models/Product.js
@@ -1,56 +1,53 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init({
-id:
+Product.init(
+  // id here to create the Primary Key
   {
-    type: DataTypes.INTEGER,
-    allowNull:false,
-    primaryKey:true,
-    autoIncrement:true
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // ============== Defined below columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: { isDecimal: true },
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: `10`,
+      validate: { isNumeric: true },
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: { model: "category", key: "id" },
+    },
   },
-  product_name:
-{
-type: DataTypes.STRING,
-allowNull:false,
-},
-
-price:
-{
-type: DataTypes.DECIMAL,
-allowNull:false,
-validate:{isDecimal:true}
-},
-
-stock:
-{
-type: DataTypes.INTEGER,
-allowNull:false,
-defaultValue:`10`,
-validate:{isNumeric:true}
-},
-
-category_id: 
-{
-type: DataTypes.INTEGER,
-references:{model: 'category',key: 'id',}
-}
- 
-},
-{
-  sequelize,
-  timestamps: false,
-  freezeTableName: true,
-  underscored: true,
-  modelName: 'product',
-}
+  // ====USING SEQUELIZE here
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product",
+  }
 );
-
+// =======Export attached here
 module.exports = Product;
